b44153fc80e9dd237308259d9c9da9e79c216da4
Github-liyifan202201 <liyifan202201@outlook.com>
2024-07-30 11:56:07 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,3 @@
-
-
 #include <bits/stdc++.h>
 #include "raylib.h"
 
@@ -59,41 +57,43 @@ void json(string a) {
 	fout.open("C:/RMCL/cmcl.json", ios::out);
 	if (fout.is_open()) {
 		fout << R"({
-	"exitWithMinecraft": false,
-	"javaPath": "C:\\Program Files\\Microsoft\\jdk-21.0.3.9-hotspot\\bin\\java.exe",
-	"windowSizeWidth": 854,
-	"windowSizeHeight": 480,
-	"language": "zh",
-	"downloadSource": 0,
-	"checkAccountBeforeStart": false,
-	"accounts": [{
-		"playerName": ")" << a << R"(",
-		"loginMethod": 0,
-		"selected": true
-	}],
-	"printStartupInfo": false
+"exitWithMinecraft": false,
+"javaPath": "C:\\Program Files\\Microsoft\\jdk-21.0.3.9-hotspot\\bin\\java.exe",
+"windowSizeWidth": 854,
+"windowSizeHeight": 480,
+"language": "zh",
+"downloadSource": 0,
+"checkAccountBeforeStart": false,
+"accounts": [{
+"playerName": ")" << a << R"(",
+"loginMethod": 2,
+"selected": true
+}],
+"printStartupInfo": false
 })";
 		fout.close();
 	}
 }
-void out(string s){
-	for(auto i:s){
-		cout<<i;
-		_sleep(50);
-	}
-}
+
 void install_java(){
+	//自动安装MSVC运行库
+	system("powershell Start-BitsTransfer https://aka.ms/vs/17/release/vc_redist.x64.exe");
+	system("vc_redist.x64.exe /install /quiet");
+	system("del vc_redist.x64.exe");
+	
+	//自动安装JDK运行库
+	
 	if(!is_e(R"(C:\RMCL\jdki.msi)")){	
-		out("正在下载java...\n");
+		
 		system(R"(powershell Start-BitsTransfer -Source "https://aka.ms/download-jdk/microsoft-jdk-21.0.3-windows-x64.msi" -Destination "C:\RMCL\jdki.msi)");
 	}
-	out("正在安装java...\n");
+	
 	system(R"(start C:\RMCL\jdki.msi /passive)"); 
 	winapi::HWND test;
 	int ti=time(0);
 	while(1){
 		if(time(0)-ti>=8){
-			out("请确定你的电脑是否支持Powershell\n安装失败\n");
+			
 			exit(0);
 		}
 		test=Get_HWND("JDK"); 
@@ -110,39 +110,67 @@ void install_java(){
 	while(!is_e(R"(C:\Program Files\Microsoft\jdk-21.0.3.9-hotspot\bin\java.exe)"))
 		_sleep(10);
 	_sleep(1000);
-	out("Jre 21 安装完成\n");
+	
 	
 	system(R"(del C:\RMCL\jdki.msi)");
 	
 }
 void install_cmcl(){
 	if(!is_e(R"(C:\RMCL\cmcl.exe)")){
-		out("正在安装CMCL内核\n");
+		
 		system(R"(powershell Start-BitsTransfer -Source "https://gitee.com/MrShiehX/console-minecraft-launcher/releases/download/2.2.1/cmcl.exe" -Destination "C:\RMCL\cmcl.exe)");	
 	}
 }
+int tps=0;
+bool ef=0;
 void install_minecraft(){
 	install_cmcl();
 	//NBSMC 附加组件
-	system(R"(powershell Start-BitsTransfer -Source "https://gitee.com/lyf202201/image/raw/main/a.zip" -Destination "C:\RMCL\fk.zip")");
-	cout<<endl;
-	system(R"(powershell Expand-Archive -Path C:\RMCL\fk.zip -DestinationPath C:\RMCL\)");
-	system(R"(del C:\RMCL\fk.zip)");
+	system(R"(powershell Start-BitsTransfer -Source "https://github.moeyy.cn/https://github.com/Github-liyifan202201/RMCL/raw/main/NBS_RMCL_add.zip" -Destination "C:\RMCL\NRA.zip")");
+	tps=15;
+	system(R"(powershell Expand-Archive -Path C:\RMCL\NRA.zip -DestinationPath C:\RMCL\)");
+	system(R"(del C:\RMCL\NRA.zip)");
+	tps=20;
+	thread a([]{
+		for(int i=0;i<70;i++){
+			if(ef==1){
+				break;
+			}
+			_sleep(9000);
+			tps++;
+		}
+	});
 	//Minecraft
-	system(R"(cd C:\RMCL & cmcl install 1.20.1 --forge=47.2.0 --optifine=HD_U_I6)");
-	system("cd /d C:\\RMCL && cmcl mod --url=https://cdn.modrinth.com/data/ordsPcFz/versions/9j6YaPp2/kotlinforforge-4.10.0-all.jar");
-	system("cd /d C:\\RMCL && cmcl mod --url=https://cdn.modrinth.com/data/qANg5Jrr/versions/bFG2YxbQ/e4mc-4.0.1%2B1.19.4-forge.jar");
-	system("cd /d C:\\RMCL && cmcl mod --url=https://cdn.modrinth.com/data/RTWpcTBp/versions/70dnR0Rh/mcwifipnp-1.6.9-1.20.1-forge.jar");
+	system(R"(cd C:\RMCL & echo y | cmcl install 1.20.1 --forge=47.3.5 --optifine=HD_U_I6)");
+	tps=90;
+	ef=1;
+	for(int i=1;i<=2;i++){
+		system("cd /d C:\\RMCL && echo 2 | cmcl mod --url=https://cdn.modrinth.com/data/ordsPcFz/versions/9j6YaPp2/kotlinforforge-4.10.0-all.jar");
+		system("cd /d C:\\RMCL && echo 2 | cmcl mod --url=https://cdn.modrinth.com/data/qANg5Jrr/versions/bFG2YxbQ/e4mc-4.0.1%2B1.19.4-forge.jar");
+		system("cd /d C:\\RMCL && echo 2 | cmcl mod --url=https://cdn.modrinth.com/data/RTWpcTBp/versions/70dnR0Rh/mcwifipnp-1.6.9-1.20.1-forge.jar");
+		system("cd /d C:\\RMCL && echo 2 | cmcl mod --url=https://cdn.modrinth.com/data/3sjzyvGR/versions/NuEoZJJF/ModernUI-Forge-1.20.1-3.10.1.4-universal.jar");
+	}
+	tps=100;
+	a.join();
 	
 }
 void launch_minecraft(){
-	system(R"(cd C:\\RMCL & cmcl 1.20.1)");
+	ofstream fout;
+	fout.open("c:\\RMCL\\la.bat");
+	fout<<"cd /d C:\\RMCL & (echo yy | cmcl 1.20.1) & exit";
+	fout.close();
+	cout<<"Start Mc!";
+//	winapi::WinExec("\"c:\\RMCL\\la.bat\"",SW_HIDE);
+//	system("C:\\RMCL\\la.bat");
+	
 }
 
 bool exitf=0;
 int main() {
+	st:;
+	if(Get_HWND("Debug")==NULL)
+		winapi::ShowWindow(winapi::GetConsoleWindow(),SW_HIDE);
 	
-	winapi::ShowWindow(winapi::GetConsoleWindow(),SW_MINIMIZE);
 	system("md c:\\RMCL");
 	// 创建一个854*480的窗口
 	InitWindow(854, 480, "RMCL");
@@ -150,51 +178,77 @@ int main() {
 	SetTargetFPS(60);
 	
 	// 创建一个文本
-	const char* studioMessage = "NBS";
-	const char* rmclMessage = "RMCL";
-	Vector2 studioTextSize = MeasureTextEx(GetFontDefault(), studioMessage, 60, 1);
-	Vector2 rmclTextSize = MeasureTextEx(GetFontDefault(), rmclMessage, 60, 1);
-	Vector2 studioTextPosition = {(float)(854 / 2 - studioTextSize.x / 2), (float)(480 / 2 - studioTextSize.y / 2)};
-	Vector2 rmclTextPosition = {(float)(854 / 2 - rmclTextSize.x / 2), (float)(480 / 2 - rmclTextSize.y / 2)};
-	Rectangle progressBar = {(float)(854 / 4), (float)(480 / 2 + 50), (float)(854 / 2), 20};
-	int progress = 0;
-	int maxProgress = 160;
+	const char* SM = "NBS";
+	const char* RM = "RMCL D1.1.5";
+	Vector2 STS = MeasureTextEx(GetFontDefault(), SM, 60, 1);
+	Vector2 RTS = MeasureTextEx(GetFontDefault(), RM, 60, 1);
+	Vector2 STP = {(float)(854 / 2 - STS.x / 2), (float)(480 / 2 - STS.y / 2)};
+	Vector2 RTP = {(float)(854 / 2 - RTS.x / 2), (float)(480 / 2 - RTS.y / 2)};
+	int PG = 0;
 	
-	
+	launch_minecraft();
 	// 启动动画
-	while (progress <= maxProgress) {
+	while (PG <= 120) {
 		
 		if(WindowShouldClose()){
 			system("taskkill /f /im consolepauser.exe");
 			exit(0);
 		}
 		// 在这里绘制启动动画
-		progress++;
-		if (progress <= 60) {
+		PG++;
+		if (PG <= 60) {
 			BeginDrawing();
 			ClearBackground(RED);
-			DrawText(studioMessage, (int)studioTextPosition.x, (int)studioTextPosition.y, 60, WHITE);
+			DrawText(SM, (int)STP.x, (int)STP.y, 60, WHITE);
+			DrawText("RMCL Dev 1.1.5 By NBS", 570, 450, 20, RAYWHITE);
 			EndDrawing();
-			progress++;
+			PG++;
 			continue;
-		}
+		} 
 		
 		
 		// 显示RMCL文本和进度条1秒
-		if (progress <= 160) {
+		if (PG <= 120) {
 			BeginDrawing();
 			ClearBackground(RED);
-			DrawText(rmclMessage, (int)rmclTextPosition.x, (int)rmclTextPosition.y, 60, WHITE);
-			DrawRectangle((int)progressBar.x, (int)progressBar.y, (int)(progressBar.width * ((float)progress - 60) / 100), (int)progressBar.height, WHITE);
+			DrawText(RM, (int)RTP.x, (int)RTP.y, 60, WHITE);
+			DrawText("RMCL Dev 1.1.5 By NBS", 570, 450, 20, RAYWHITE);
 			EndDrawing();
-			progress++;
+			PG++;
+			
 			continue;
 		}
+		if(!is_e(R"(C:\RMCL\.minecraft\versions\1.20.1\1.20.1.jar)")){	
+			// 结束启动动画
+			break;
+		}
+		else{
+			winapi::HWND mc=Get_HWND("Minecraft");
+			while(mc==NULL){
+				
+				if(WindowShouldClose()){
+					system("taskkill /f /im consolepauser.exe");
+					exit(0);
+				}
+				mc=Get_HWND("Minecraft");
+				BeginDrawing();
+				EndDrawing();
+			}
+			break;
+		}
 		
-		// 结束启动动画
-		break;
 	}
-	
+	/*
+	自动更新
+	system("curl curl -O https://github.moeyy.xyz/https://github.com/Github-liyifan202201/RMCL/releases/download/v1.2/RMCL.exe");
+	ifstream fin;
+	fin.open("RMCL.exe");
+	string a;
+	fin>>a;
+	if(a!="Not"){
+	cout<<"请更新版本";
+	}
+	*/
 	// 检查文件是否存在
 	if (!is_e("C:/RMCL/cmcl.json")) {
 		// 如果文件不存在，则创建一个居中的输入框
@@ -213,8 +267,8 @@ int main() {
 			BeginDrawing();
 			ClearBackground(RED);
 			DrawText(inputPrompt, (int)inputBoxPosition.x, (int)inputBoxPosition.y - 30, 20, WHITE);
-			DrawRectangle((int)inputBoxPosition.x, (int)inputBoxPosition.y, 400, 40, LIGHTGRAY);
-			DrawText(name.c_str(), (int)inputBoxPosition.x + 10, (int)inputBoxPosition.y + 10, 20, WHITE);
+			DrawRectangle((int)inputBoxPosition.x, (int)inputBoxPosition.y, 400, 40, RAYWHITE);
+			DrawText(name.c_str(), (int)inputBoxPosition.x + 10, (int)inputBoxPosition.y + 10, 20, BLACK);
 			
 			// 绘制勾
 			DrawText("OK", (int)checkmarkPosition.x - 14, (int)checkmarkPosition.y + 5, 30, GREEN);
@@ -243,25 +297,27 @@ int main() {
 			}
 		}
 	}
-	
+	//下载Java
 	SetTargetFPS(3);
 	if(!is_e(R"(C:\Program Files\Microsoft\jdk-21.0.3.9-hotspot\bin\java.exe)")){
 		exitf=0;
 		thread a([]{
 			install_java();
 			system("java --version");
-			out("OK\n");
+			
 			exitf=1;
 		});
 		
-		if(WindowShouldClose()){
-			system("taskkill /f /im consolepauser.exe");
-			exit(0);
-		}
+		
 		while(!exitf){
+			if(WindowShouldClose()){
+				system("taskkill /f /im consolepauser.exe");
+				exit(0);
+			}
 			BeginDrawing();
 			ClearBackground(RED);
 			DrawText("Install MS-JDK-21.", 250, 200, 40, WHITE);
+			
 			EndDrawing();
 			
 			BeginDrawing();
@@ -284,12 +340,13 @@ int main() {
 	}
 	if (!is_e(R"(C:\RMCL\.minecraft\versions\1.20.1\1.20.1.jar)")) {
 		exitf=0;
+		
 		thread a([]{
 			cout<<"Install Minecraft...";
 			if(!is_e(R"(C:\RMCL\.minecraft\versions\1.20.1\1.20.1.jar)")){
-				out("正在安装Minecraft 1.20.1 (NBS MC 3.0)\n---\n");
+				
 				install_minecraft();
-				out("OK\n");
+				
 			}
 			exitf=1;
 		});
@@ -302,39 +359,38 @@ int main() {
 			BeginDrawing();
 			ClearBackground(RED);
 			DrawText("Install Minecraft.", 250, 200, 40, WHITE);
+			string s="Installation Progress ("+to_string(tps)+"%)";
+			DrawText(s.c_str(), 550, 450, 20, RAYWHITE);
 			EndDrawing();
 			
 			BeginDrawing();
 			ClearBackground(RED);
+			
 			DrawText("Install Minecraft..", 250, 200, 40, WHITE);
+			s="Installation Progress ("+to_string(tps)+"%)";
+			DrawText(s.c_str(), 550, 450, 20, RAYWHITE);
 			EndDrawing();
 			
 			BeginDrawing();
 			ClearBackground(RED);
+			
 			DrawText("Install Minecraft...", 250, 200, 40, WHITE);
+			s="Installation Progress ("+to_string(tps)+"%)";
+			DrawText(s.c_str(), 550, 450, 20, RAYWHITE);
 			EndDrawing();
 			
 			BeginDrawing();
 			ClearBackground(RED);
 			DrawText("Install Minecraft", 250, 200, 40, WHITE);
+			s="Installation Progress ("+to_string(tps)+"%)";
+			DrawText(s.c_str(), 550, 450, 20, RAYWHITE);
 			EndDrawing();
 		}
 		a.join();
 		
 	}
-	launch_minecraft();
-	while (Get_HWND("Minecraft")==NULL) {
-		
-		if(WindowShouldClose()){
-			system("taskkill /f /im consolepauser.exe");
-			exit(0);
-		}
-		BeginDrawing();
-		ClearBackground(RED);
-		DrawText("NBS - Minecraft", 250, 200, 40, WHITE);
-		EndDrawing();
-	}
-	CloseWindow();
+	goto st;
+	
 	system("taskkill /f /im consolepauser.exe");
 	return 0;
 }
